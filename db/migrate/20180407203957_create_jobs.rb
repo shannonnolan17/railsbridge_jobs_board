eece1ab89082055bb43c43b14a2f4a0417e619e9
Shannon Nolan <shannonnolan17@gmail.com>
2018-04-07 14:17:05 -0700

    Create creat routes

---
+++ db/migrate/20180407203957_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs do |t|
+
+        t.text :title
+        t.text :description
+
+      t.timestamps
+    end
+  end
+end
